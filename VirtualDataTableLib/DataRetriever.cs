0d4c5820c9eabc452f8ff3f8e9a89e1ddbaf7493
Juarez Rudsatz <juarez.rudsatz@ceabs.com.br>
2017-11-30 12:36:18 -0200

    File Formats: Support properties

--- VirtualDataTableLib/DataRetriever.cs
+++ VirtualDataTableLib/DataRetriever.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
 
 // Based on: https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/virtual-mode-with-just-in-time-data-loading-in-the-datagrid
@@ -12,21 +14,40 @@ namespace VirtualDataTableLib
         long? GetTotalRowCount();
 
         void OpenDataSource(string sourceAddress);
+
+        IDictionary<string, string> GetProperties();
     }
 
     public abstract class DataRetriever : IDataPageRetriever
     {
+        private IDictionary<string, string> _properties = new Dictionary<string, string>();
+
         public abstract long? GetTotalRowCount();
 
         public abstract DataTable SupplyPageOfData(int lowerPageBoundary, int rowsPerPage);
 
         public abstract void OpenDataSource(string sourceAddress);
 
+        public IDictionary<string, string> GetProperties()
+        {
+            return _properties;
+        }
+
+        public void SetProperty(string key, string value)
+        {
+            _properties[key] = value;
+        }
     }
 
-    public abstract class FileDataRetriever : DataRetriever
+    public abstract class FileDataRetriever : DataRetriever, IDisposable
     {
         private Stream fileStream;
+
+        public virtual void Dispose()
+        {
+            if (fileStream != null)
+                ((IDisposable)fileStream).Dispose();
+        }
 
         public override void OpenDataSource(string sourceAddress)
         {
